0b5ff430fa8226b6ca00e710233cf1a4ca6537b8
chiragkshahdev <chiragkiranshahshah@gmail.com>
2026-09-17 13:42:24 +0530

    "Day 03: 3D Tilt Card"

---
+++ Day-03-3D-Tilt-Card/script.js
@@ -0,0 +1,17 @@
+const card =document.getElementById('card');
+const container = document.querySelector('.container');
+
+container.addEventListener('mousemove', (e) => {
+    let xAxis = (window.innerWidth / 2 - e.pageX) / 15;
+    let yAxis = (window.innerHeight / 2 - e.pageY) / 15;
+    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+
+container.addEventListener('mouseenter', () => {
+    card.style.transition = 'none';
+});
+
+container.addEventListener('mouseleave', () => {
+    card.style.transition = 'all 0.5s ease';
+    card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+});
